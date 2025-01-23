8b00cb7bd00411d379d6fa481ddacd2d50899de4
narisarit <rast777@naver.com>
2025-01-23 17:25:29 +0900

    codes for inference and modeling

--- inference-and-modeling.R
+++ inference-and-modeling.R
@@ -1,1 +1,15 @@
-a
+take_poll()
+sample()
+replicate()
+grid.arrange()
+sapply()
+between()
+round()
+ungroup()
+qt()
+pt()
+qnorm()
+pnorm()
+
+fisher.test() #Fisher's Exact Test 
+chisq_test() #Chi-squared test
